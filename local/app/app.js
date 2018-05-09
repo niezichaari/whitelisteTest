b576fa57235547d4b99fbd588c3c7d4bc4b17d39
Niazi_Chaari <niezi.ch@gmail.com>
2018-05-09 17:41:59 +0200

    third commit

--- local/app/app.js
+++ local/app/app.js
@@ -1,17 +1,48 @@
 var socket = io.connect('http://localhost:4050');
-socket.on('data', function (data) {
+socket.on('data', function(data) {
   ReactDOM.render(React.createElement(table, data), document.getElementById('app'));
 });
 
 const table = createReactClass({
   render: function() {
-/*    return React.createElement("table", null,
-      React.createElement("tbody", null,
-        React.createElement("tr", null,
+    var cols = [
+        {  label: 'Id ' },
+        {  label: 'Url ' },
+        {  label: 'User_agent ' },
+        {  label: 'Blocked ' }
+    ];
+
+
+        return React.createElement("table", null,
+          React.createElement("body", null,
+            React.createElement("tr", null,
+             cols.map(function(colData) {
+               return React.createElement ("th", null,colData.label)})
+
+          ),
           React.createElement("td", null,
-            "hey")
+           this.props.results.map(function(colData) {
+             return React.createElement ("tr", null,colData.id)})
+
         )
-      )
-    );*/ return React.createElement("p", null,JSON.stringify(this.props))
-  }
+          ,
+          React.createElement("td", null,
+           this.props.results.map(function(colData) {
+             return React.createElement ("tr", null,colData.url)})
+
+        ),
+        React.createElement("td", null,
+         this.props.results.map(function(colData) {
+           return React.createElement ("tr", null,colData.user_agent)})
+
+      ),
+      React.createElement("td", null,
+       this.props.results.map(function(colData) {
+         return React.createElement ("tr", null,colData.blocked)})
+
+    )
+        ))
+        //console.log(this.props.results[0]);
+  //  return React.createElement("p", null, JSON.stringify(this.props))
+}
 })
